44cbe82f78cf3e83318dfaa391ea585441b8cc46
jakem1027 <jmartin89@gmail.com>
2021-01-24 15:56:37 -0500

    removed structured data

--- _06_data_preprocessing_test.py
+++ _06_data_preprocessing_test.py
@@ -23,15 +23,20 @@ def sheepish_mkdir(path):
 dirs = ["/Users/martijac/Documents/Frailty/frailty_classifier/output/",
         "/media/drv2/andrewcd2/frailty/output/",
         "/share/gwlab/frailty/output/"]
+
 for d in dirs:
     if os.path.exists(d):
         datadir = d
+
 if datadir == dirs[0]:  # mb
     notesdir = datadir
+
 if datadir == dirs[1]:  # grace
     notesdir = f"{os.getcwd()}/output/"
+
 if datadir == dirs[2]:  # azure
     notesdir = datadir
+
 outdir = f"{datadir}notes_preprocessed_SENTENCES_test/"
 SVDdir = f"{outdir}svd/"
 embeddingsdir = f"{outdir}embeddings/"
@@ -44,23 +49,18 @@ sheepish_mkdir(trtedatadir)
 # load SENTENCES
 # check for .csv in filename to avoid the .DSstore file
 # load the notes from 2018
-notes_2018 = [i for i in
-              os.listdir(notesdir + "notes_labeled_embedded_SENTENCES/")
-              if '.csv' in i and int(i.split("_")[-2][1:]) < 13]
+notes_2018 = [i for i in os.listdir(notesdir + "notes_labeled_embedded_SENTENCES/") if '.csv' in i and int(i.split("_")[-2][1:]) < 13]
 # drop the notes that aren't in the concatenated notes data frame
 # some notes got labeled and embedded but were later removed from the pipeline
 # on July 14 2020, due to the inclusion of the 12-month ICD lookback
 cndf = pd.read_pickle(f"{datadir}conc_notes_df.pkl")
 cndf = cndf.loc[cndf.LATEST_TIME < "2019-01-01"]
-cndf['month'] = cndf.LATEST_TIME.dt.month + (
-        cndf.LATEST_TIME.dt.year - min(cndf.LATEST_TIME.dt.year)) * 12
+cndf['month'] = cndf.LATEST_TIME.dt.month + (cndf.LATEST_TIME.dt.year - min(cndf.LATEST_TIME.dt.year)) * 12
 # generate 'note' label (used in webanno and notes_labeled_embedded)
 uidstr = ("m" + cndf.month.astype(str) + "_" + cndf.PAT_ID + ".csv").tolist()
 # conc_notes_df contains official list of eligible patients
-notes_2018_in_cndf = [i for i in notes_2018 if
-                      "_".join(i.split("_")[-2:]) in uidstr]
-notes_excluded = [i for i in notes_2018 if
-                  "_".join(i.split("_")[-2:]) not in uidstr]
+notes_2018_in_cndf = [i for i in notes_2018 if "_".join(i.split("_")[-2:]) in uidstr]
+notes_excluded = [i for i in notes_2018 if "_".join(i.split("_")[-2:]) not in uidstr]
 
 #get culled repeat notes
 culled_repeats = list(pd.read_csv(f"{datadir}cull_list_2021_01_24.txt").iloc[:, 1])
@@ -68,88 +68,12 @@ notes_2018_in_cndf_culled = set(notes_2018_in_cndf).difference(culled_repeats)
 
 assert len(notes_2018_in_cndf_culled) + len(notes_excluded) + len(culled_repeats) == len(notes_2018)
 # get notes_labeled_embedded that match eligible patients only
-df = pd.concat(
-    [pd.read_csv(notesdir + "notes_labeled_embedded_SENTENCES/" + i,
-    dtype={'sent_start': 'boolean', 'token': 'string', 'PAT_ID': 'string'}) for i in
-     notes_2018_in_cndf_culled])
+df = pd.concat([pd.read_csv(notesdir + "notes_labeled_embedded_SENTENCES/" + i, dtype={'sent_start': 'boolean', 'token': 'string', 'PAT_ID': 'string'}) for i in notes_2018_in_cndf_culled])
 df.drop(columns='Unnamed: 0', inplace=True)
 # reset the index
 df2 = df.reset_index(drop=True)
 # set seed
 seed = 111120
-
-# define some useful constants
-str_varnames = ['n_encs', 'n_ed_visits', 'n_admissions', 'days_hospitalized',
-                'mean_sys_bp', 'mean_dia_bp', 'sd_sys_bp', 'sd_dia_bp',
-                'bmi_mean', 'bmi_slope', 'max_o2', 'spo2_worst', 'ALBUMIN',
-                'ALKALINE_PHOSPHATASE', 'AST', 'BILIRUBIN', 'BUN', 'CALCIUM',
-                'CO2', 'CREATININE', 'HEMATOCRIT', 'HEMOGLOBIN', 'LDL', 'MCHC',
-                'MCV', 'PLATELETS', 'POTASSIUM', 'PROTEIN', 'RDW', 'SODIUM',
-                'WBC', 'sd_ALBUMIN', 'sd_ALKALINE_PHOSPHATASE', 'sd_AST',
-                'sd_BILIRUBIN', 'sd_BUN', 'sd_CALCIUM', 'sd_CO2',
-                'sd_CREATININE', 'sd_HEMATOCRIT', 'sd_HEMOGLOBIN', 'sd_LDL',
-                'sd_MCHC', 'sd_MCV', 'sd_PLATELETS', 'sd_POTASSIUM',
-                'sd_PROTEIN', 'sd_RDW', 'sd_SODIUM', 'sd_WBC', 'n_ALBUMIN',
-                'n_ALKALINE_PHOSPHATASE', 'n_AST', 'n_BILIRUBIN', 'n_BUN',
-                'n_CALCIUM', 'n_CO2', 'n_CREATININE', 'n_HEMATOCRIT',
-                'n_HEMOGLOBIN', 'n_LDL', 'n_MCHC', 'n_MCV', 'n_PLATELETS',
-                'n_POTASSIUM', 'n_PROTEIN', 'n_RDW', 'n_SODIUM', 'n_WBC',
-                'FERRITIN', 'IRON', 'MAGNESIUM', 'TRANSFERRIN',
-                'TRANSFERRIN_SAT', 'sd_FERRITIN', 'sd_IRON', 'sd_MAGNESIUM',
-                'sd_TRANSFERRIN', 'sd_TRANSFERRIN_SAT', 'n_FERRITIN', 'n_IRON',
-                'n_MAGNESIUM', 'n_TRANSFERRIN', 'n_TRANSFERRIN_SAT', 'PT',
-                'sd_PT', 'n_PT', 'PHOSPHATE', 'sd_PHOSPHATE', 'n_PHOSPHATE',
-                'PTT', 'sd_PTT', 'n_PTT', 'TSH', 'sd_TSH', 'n_TSH',
-                'n_unique_meds', 'elixhauser', 'n_comorb', 'AGE', 'SEX_Female',
-                'SEX_Male', 'MARITAL_STATUS_Divorced', 'MARITAL_STATUS_Married',
-                'MARITAL_STATUS_Other', 'MARITAL_STATUS_Single',
-                'MARITAL_STATUS_Widowed', 'EMPY_STAT_Disabled',
-                'EMPY_STAT_Full Time', 'EMPY_STAT_Not Employed',
-                'EMPY_STAT_Other', 'EMPY_STAT_Part Time', 'EMPY_STAT_Retired',
-                'MV_n_encs', 'MV_n_ed_visits', 'MV_n_admissions',
-                'MV_days_hospitalized', 'MV_mean_sys_bp', 'MV_mean_dia_bp',
-                'MV_sd_sys_bp', 'MV_sd_dia_bp', 'MV_bmi_mean', 'MV_bmi_slope',
-                'MV_max_o2', 'MV_spo2_worst', 'MV_ALBUMIN',
-                'MV_ALKALINE_PHOSPHATASE', 'MV_AST', 'MV_BILIRUBIN', 'MV_BUN',
-                'MV_CALCIUM', 'MV_CO2', 'MV_CREATININE', 'MV_HEMATOCRIT',
-                'MV_HEMOGLOBIN', 'MV_LDL', 'MV_MCHC', 'MV_MCV', 'MV_PLATELETS',
-                'MV_POTASSIUM', 'MV_PROTEIN', 'MV_RDW', 'MV_SODIUM', 'MV_WBC',
-                'MV_sd_ALBUMIN', 'MV_sd_ALKALINE_PHOSPHATASE', 'MV_sd_AST',
-                'MV_sd_BILIRUBIN', 'MV_sd_BUN', 'MV_sd_CALCIUM', 'MV_sd_CO2',
-                'MV_sd_CREATININE', 'MV_sd_HEMATOCRIT', 'MV_sd_HEMOGLOBIN',
-                'MV_sd_LDL', 'MV_sd_MCHC', 'MV_sd_MCV', 'MV_sd_PLATELETS',
-                'MV_sd_POTASSIUM', 'MV_sd_PROTEIN', 'MV_sd_RDW',
-                'MV_sd_SODIUM', 'MV_sd_WBC', 'MV_n_ALBUMIN',
-                'MV_n_ALKALINE_PHOSPHATASE', 'MV_n_AST', 'MV_n_BILIRUBIN',
-                'MV_n_BUN', 'MV_n_CALCIUM', 'MV_n_CO2', 'MV_n_CREATININE',
-                'MV_n_HEMATOCRIT', 'MV_n_HEMOGLOBIN', 'MV_n_LDL', 'MV_n_MCHC',
-                'MV_n_MCV', 'MV_n_PLATELETS', 'MV_n_POTASSIUM', 'MV_n_PROTEIN',
-                'MV_n_RDW', 'MV_n_SODIUM', 'MV_n_WBC', 'MV_FERRITIN',
-                'MV_IRON', 'MV_MAGNESIUM', 'MV_TRANSFERRIN',
-                'MV_TRANSFERRIN_SAT', 'MV_sd_FERRITIN', 'MV_sd_IRON',
-                'MV_sd_MAGNESIUM', 'MV_sd_TRANSFERRIN',
-                'MV_sd_TRANSFERRIN_SAT', 'MV_n_FERRITIN', 'MV_n_IRON',
-                'MV_n_MAGNESIUM', 'MV_n_TRANSFERRIN', 'MV_n_TRANSFERRIN_SAT',
-                'MV_PT', 'MV_sd_PT', 'MV_n_PT', 'MV_PHOSPHATE',
-                'MV_sd_PHOSPHATE', 'MV_n_PHOSPHATE', 'MV_PTT', 'MV_sd_PTT',
-                'MV_n_PTT', 'MV_TSH', 'MV_sd_TSH', 'MV_n_TSH',
-                'MV_n_unique_meds', 'MV_elixhauser', 'MV_n_comorb', 'MV_AGE',
-                'MV_SEX', 'MV_MARITAL_STATUS', 'MV_EMPY_STAT']
-out_varnames = ['Msk_prob', 'Nutrition', 'Resp_imp', 'Fall_risk']
-
-# #check for completely missing data (repeat after using ACD updated structured
-# # data scripts
-# str_varnames2 = str_varnames.copy()
-# str_varnames2.append('note')
-# missing_note = df2[str_varnames2].groupby('note').apply(lambda x: x.isnull().mean())
-# missing_note = missing_note.mean(axis=1)
-# list(missing_note.loc[missing_note > 0.9].index)
-
-# drop the mean & sd for labs that are >70% missing. Keep the missing value
-# indicator (so rare labs become present/absent)
-missingness = df2[str_varnames].isnull().sum()/df2.shape[0]
-missingness = missingness.loc[missingness > 0.7].index
-str_varnames = np.setdiff1d(str_varnames, missingness)
 
 # set a unique sentence id that does not reset to 0 with each note
 sentence = []
@@ -219,8 +143,7 @@ embeddings2 = embeddings.loc[:,
 
 # make df of structured data and labels
 str_lab = df2.loc[:, df2.columns.isin(['PAT_ID', 'note', 'month',
-                                           'sentence_id']) |
-                     df2.columns.isin(str_varnames)].copy()
+                                           'sentence_id'])].copy()
 # get one row of structured data for each sentence
 str_lab = str_lab.groupby('sentence_id', as_index=True).first().reset_index(drop=False)
 #check that sentence_ids match
