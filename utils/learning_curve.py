fdf4e867cc85d70298c52eafa96908eb8031d2f3
jakem1027 <jmartin89@gmail.com>
2021-04-07 11:27:23 -0400

    simplified summarization of multiNN

--- utils/learning_curve.py
+++ utils/learning_curve.py
@@ -32,41 +32,9 @@ def consolidate_NN_perf():
         multi = pd.DataFrame(xd)
         multi.shape
         # mean of multi-class scaled briers for all aspects
-        multi['brier_mean_aspects'] = multi.loc[:,['Fall_risk', 'Msk_prob', 'Nutrition', 'Resp_imp']].mean(axis = 1)
-        # summarize by repeat/fold
-        m_col = ['n_dense', 'n_units', 'dropout', 'l1_l2_pen',
-           'use_case_weights', 'Fall_risk_neg', 'Fall_risk_neut',
-           'Fall_risk_pos', 'Msk_prob_neg', 'Msk_prob_neut', 'Msk_prob_pos',
-           'Nutrition_neg', 'Nutrition_neut', 'Nutrition_pos', 'Resp_imp_neg',
-           'Resp_imp_neut', 'Resp_imp_pos', 'Fall_risk', 'Msk_prob', 'Nutrition',
-           'Resp_imp', 'brier_mean_aspects']
-        hyper_cols = ['n_dense', 'n_units', 'dropout', 'l1_l2_pen', 'use_case_weights']
-        hyperparam_mean = multi[m_col].groupby(hyper_cols,
-                                               as_index=False).mean()
-        news = [(i, i + '_mean') for i in hyperparam_mean.iloc[:, 5:].columns.values]
-        hyperparam_mean.rename(columns = dict(news), inplace=True)
-        hyperparam_se = multi[m_col].groupby(hyper_cols,
-                                             as_index=False).sem()
-        news = [(i, i + '_se') for i in hyperparam_se.iloc[:, 5:].columns.values]
-        hyperparam_se.rename(columns = dict(news), inplace=True)
-        hyperparam_perf = hyperparam_mean.merge(hyperparam_se, on=hyper_cols)
-        hyperparam_median = multi[m_col].groupby(hyper_cols,
-                                                 as_index=False).median()
-        news = [(i, i + '_median') for i in hyperparam_median.iloc[:, 5:].columns.values]
-        hyperparam_median.rename(columns = dict(news), inplace=True)
-        hyperparam_perf = hyperparam_perf.merge(hyperparam_median, on=hyper_cols)
-        hyperparam_iqr25 = multi[m_col].groupby(hyper_cols,
-                                                as_index=False).quantile(0.25)
-        news = [(i, i + '_iqr25') for i in hyperparam_iqr25.iloc[:, 5:].columns.values]
-        hyperparam_iqr25.rename(columns = dict(news), inplace=True)
-        hyperparam_perf = hyperparam_perf.merge(hyperparam_iqr25, on=hyper_cols)
-        hyperparam_iqr75 = multi[m_col].groupby(hyper_cols,
-                                                as_index=False).quantile(0.75)
-        news = [(i, i + '_iqr75') for i in hyperparam_iqr75.iloc[:, 5:].columns.values]
-        hyperparam_iqr75.rename(columns = dict(news), inplace=True)
-        hyperparam_perf = hyperparam_perf.merge(hyperparam_iqr75, on=hyper_cols)
-        hyperparam_perf['batch'] = batch
-        mult_b.append(hyperparam_perf)
+        multi['brier_mean_aspects'] = multi.loc[:, ['Fall_risk', 'Msk_prob', 'Nutrition', 'Resp_imp']].mean(axis = 1)
+        multi['batch'] = batch
+        mult_b.append(multi)
         sd = []
         for pkl in single:
             x = read_pickle(f"{pklpath}{pkl}")
